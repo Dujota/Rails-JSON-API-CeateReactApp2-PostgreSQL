81213686ebad371da9509e63be70dff951d54331
Denis <ddujota@internationalsafety.com>
2019-03-27 17:51:00 -0400

    finished with the rails setup and linter

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,5 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-List.create(title: "West Sweden Road Trip", excerpt: "A cool road trip with stops in harbors of the coast")
-List.create(title: "Must have equipment for the outdoor photographer", excerpt: "My selection of gear for modern outdoor photography")
+List.create(title: "Interesting Project Ideas", excerpt: "A shortlist of interesting ideas and concepts for projects")
+List.create(title: "Must Learn List", excerpt: "My selection of frameworks/languages to learn in 2019")
