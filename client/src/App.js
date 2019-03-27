21e5e4a66c0c6a986cecdd02145418c22dfea179
Denis <ddujota@internationalsafety.com>
2019-03-27 16:15:20 -0400

    fixed all cors issues

---
+++ client/src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './App.css';
+import ListsContainer from './components/ListContainer';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Hello Hiplyst!</h1>
+        </header>
+        <ListsContainer />
+      </div>
+    );
+  }
+}
+
+export default App;
